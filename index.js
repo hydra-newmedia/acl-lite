6c6159cc41e95dfcaa40157b4b947df42e064332
Christian Hotz <hotz@hydra-newmedia.com>
2016-11-03 16:56:11 +0100

    restructuring of recursive methods and outsourcing of a frequently used functionality

--- index.js
+++ index.js
@@ -25,22 +25,16 @@ class Role {
       throw new TypeError('permission must be of type array or string');
 
     // set permission object as tree
-    this.deepAddPermission(permission);
-  }
-
-  /**
-   * Recursive method to add object tree of permission to this.permissions
-   * @param {Array.<string>} path - path to be added to the permissions
-   * @param {Object} [permissions=this.permissions] - permissions object new permissions should be added to
-   */
-  deepAddPermission(path, permissions = this.permissions) {
-    if (permissions === true) return;
-    let key = path[0];
-    if (path.length === 1)
-      permissions[key] = true;
-    else
-      permissions[key] = permissions[key] || {};
-    this.deepAddPermission(path.slice(1), permissions[key]);
+    const recursiveAddPermission = (path, permissions = this.permissions) => {
+      if (permissions === true) return;
+      let key = path[0];
+      if (path.length === 1)
+        permissions[key] = true;
+      else
+        permissions[key] = permissions[key] || {};
+      recursiveAddPermission(path.slice(1), permissions[key]);
+    };
+    recursiveAddPermission(permission);
   }
 
   /**
@@ -54,21 +48,14 @@ class Role {
     else if (!(permission instanceof Array))
       throw new TypeError('permission must be of type array or string');
 
-    return this.deepHasPermission(permission);
-  }
-
-  /**
-   * Recursive method for finding whether a permission exists.
-   * @param {Array.<string>} path - permission to search for
-   * @param {Object} [permissions=this.permissions] - permissions object to search in
-   * @returns {boolean} - true if subobject with value true exists on traversing the `path`
-   */
-  deepHasPermission(path, permissions = this.permissions) {
-    if (path.length === 0) return false;
-    let key = path[0];
-    if (path.length === 1) return !!permissions[key];
-    if (permissions[key] === true) return true;
-    else return this.deepHasPermission(path.slice(1), permissions[key]);
+    const recursiveHasPermission = (path, permissions = this.permissions) => {
+      if (path.length === 0) return false;
+      let key = path[0];
+      if (path.length === 1) return !!permissions[key];
+      if (permissions[key] === true) return true;
+      else return recursiveHasPermission(path.slice(1), permissions[key]);
+    };
+    return recursiveHasPermission(permission);
   }
 
   /**
@@ -78,23 +65,20 @@ class Role {
    * @returns {boolean|string} - true if object satisfies permissions, false if it's no object or
    * a path {string} of the first property, which is not permitted
    */
-  checkObject(object, path = null) {
-    if (path && typeof path === 'string')
-      path = path.split('.');
-    else if (path && !(path instanceof Array))
-      throw new TypeError('path must be of type array or string');
-
-    if (path) {
-      let permissions = this.permissions;
-      for (let key of path) {
-        if (!(key in permissions))
-          throw new Error('invalid path to sub-permission', 'InvalidPermissionPathError');
-        permissions = permissions[key];
+  checkObject(object, path = undefined) {
+    const recursiveCheckObject = (object, permissions, path = '') => {
+      if (!permissions) return path;
+      if (object.constructor === {}.constructor) {
+        for (let key in object) {
+          const subCheck = recursiveCheckObject(object[key], permissions[key], path + (path === '' ? key : '.' + key));
+          if (subCheck !== true) return subCheck;
+        }
+      } else if (path === '') {
+        return false;
       }
-      return this.deepCheckObject(object, permissions);
-    } else {
-      return this.deepCheckObject(object, this.permissions);
-    }
+      return true;
+    };
+    return recursiveCheckObject(object, this.getSubPermission(path));
   }
 
   /**
@@ -104,18 +88,6 @@ class Role {
    * @param {string} [path='']
    * @returns {boolean|string} - path when aborting, false if non-object, true if completely permitted
    */
-  deepCheckObject(object, permissions, path = '') {
-    if (!permissions) return path;
-    if (object.constructor === {}.constructor) {
-      for (let key in object) {
-        const subCheck = this.deepCheckObject(object[key], permissions[key], path + (path === '' ? key : '.' + key));
-        if (subCheck !== true) return subCheck;
-      }
-    } else if (path === '') {
-      return false;
-    }
-    return true;
-  }
 
   /**
    * Get (sub-)permissions in a flatted object.
@@ -123,20 +95,8 @@ class Role {
    * @param {*} [value=true] - value the flatted object should hold, eg. 1 for mongoose filtering
    * @returns {Object} - flatted (sub-)permissions object
    */
-  getFlatPermissions(path = null, value = true) {
-    if (path && typeof path === 'string')
-      path = path.split('.');
-    else if (path && !(path instanceof Array))
-      throw new TypeError('path must be of type array or string');
-    else if (!path)
-      path = [];
-
-    let permissions = this.permissions;
-    for (let key of path) {
-      if (!(key in permissions))
-        throw new Error('invalid path to sub-permission', 'InvalidPermissionPathError');
-      permissions = permissions[key];
-    }
+  getFlatPermissions(path = undefined, value = true) {
+    let permissions = this.getSubPermission(path);
 
     let flatted = {};
     let deepFlatten = (object, path = '') => {
@@ -150,6 +110,23 @@ class Role {
     };
     deepFlatten(permissions);
     return flatted;
+  }
+
+  getSubPermission(path = null) {
+    if (!path)
+      path = [];
+    else if (typeof path === 'string')
+      path = path.split('.');
+    else if (!(path instanceof Array))
+      throw new TypeError('path must be of type array or string');
+
+    let permissions = this.permissions;
+    for (let key of path) {
+      if (!(key in permissions))
+        throw new Error('invalid path to sub-permission', 'InvalidPermissionPathError');
+      permissions = permissions[key];
+    }
+    return permissions;
   }
 
 }
